bae03380a98d4ce726181992f8ad7a382c118de1
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-09-21 12:50:14 -0500

    Starting with TypeScript

---
+++ bases/app.js
@@ -0,0 +1,11 @@
+"use strict";
+const msg = "Hola mundo";
+const hero = {
+    name: "Ironman",
+    age: 45,
+};
+hero.age = 50;
+console.log(msg);
+console.log(hero);
+console.log(hero.name);
+console.log(hero.age);
---
+++ bases/app.ts
@@ -0,0 +1,13 @@
+const msg: string = "Hola mundo";
+
+const hero = {
+   name: "Ironman",
+   age: 45,
+};
+
+hero.age = 50;
+
+console.log(msg);
+console.log(hero);
+console.log(hero.name);
+console.log(hero.age);
